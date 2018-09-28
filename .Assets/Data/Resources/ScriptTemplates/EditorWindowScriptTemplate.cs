db88c2439ccacf6667e4dc75a6fdbb6633f66a69
DESKTOP-8FL3M3V\Alan <835988221@qq.com>
2018-09-28 18:27:34 +0800

    修改EditorWindow的模板文件。

--- .Assets/Data/Resources/ScriptTemplates/EditorWindowScriptTemplate.cs
+++ .Assets/Data/Resources/ScriptTemplates/EditorWindowScriptTemplate.cs
@@ -1,13 +1,8 @@
-﻿//----------------------------------------------------
-//Copyright © 2008-2018 Mr-Alan. All rights reserved.
-//Mail: Mr.Alan.China@[outlook|gmail].com
-//Website: www.0x69h.com
-//----------------------------------------------------
-
 using UnityEditor;
 using UnityEngine;
+using BlackFire.Editor;
 
-namespace BlackFireFramework.Editor
+namespace BlackFire.Editor
 {
     public sealed class #ClassName# : EditorWindowBase<#ClassName#>
     {
