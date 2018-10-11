8a91bc0ae9fcb1ebda406a929214e1f6b033fe3d
DESKTOP-8FL3M3V\Alan <835988221@qq.com>
2018-10-11 15:44:33 +0800

    添加Date辅助类。

---
+++ Runtime/Script/Common/Utility/Utility.Date.cs
@@ -0,0 +1,51 @@
+﻿//----------------------------------------------------
+//Copyright © 2008-2018 Mr-Alan. All rights reserved.
+//Mail: Mr.Alan.China@[outlook|gmail].com
+//Website: www.0x69h.com
+//----------------------------------------------------
+
+using System;
+using System.Net;
+
+namespace BlackFire.Unity
+{
+    public static partial class Utility
+    {
+        public static class Date
+        {
+            /// <summary>  
+            /// 获取网络日期时间  
+            /// </summary>  
+            /// <returns>网络时间字符串。</returns>  
+            public static string AcquireNetDateTime()  
+            {  
+                WebRequest request = null;  
+                WebResponse response = null;  
+                WebHeaderCollection headerCollection = null;  
+                string datetime = string.Empty;  
+                try  
+                {  
+                    request = WebRequest.Create("https://www.baidu.com");  
+                    request.Timeout = 3000;  
+                    request.Credentials = CredentialCache.DefaultCredentials;  
+                    response = (WebResponse)request.GetResponse();  
+                    headerCollection = response.Headers;  
+                    foreach (var h in headerCollection.AllKeys)  
+                    { if (h == "Date") { datetime = headerCollection[h]; break; } }  
+                    return datetime;  
+                }  
+                catch (Exception) { return datetime; }  
+                finally  
+                {  
+                    if (request != null)  
+                    { request.Abort(); }  
+                    if (response != null)  
+                    { response.Close(); }  
+                    if (headerCollection != null)  
+                    { headerCollection.Clear(); }  
+                }  
+            }
+        }
+
+    }
+}
