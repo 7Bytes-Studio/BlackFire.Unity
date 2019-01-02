147f2b057569b700feb7588a353c02c5bbc566f0
0x69h <mr.alan.china@gmail.com>
2019-01-02 09:54:25 +0800

    确认同步。

---
+++ Runtime/Script/Common/Utility/Utility.Math.cs
@@ -0,0 +1,30 @@
+﻿/*
+--------------------------------------------------
+| Copyright © 2008 Mr-Alan. All rights reserved. |
+| Website: www.0x69h.com                         |
+| Mail: mr.alan.china@gmail.com                  |
+| QQ: 835988221                                  |
+--------------------------------------------------
+*/
+
+namespace BlackFire.Unity
+{
+    public static partial class Utility
+    {
+        
+        public static class Math
+        {
+
+            public static bool Within(float value,float min,float max)
+            {
+                return min <= value && value <= max;
+            }
+
+
+
+
+
+        }
+
+    }
+}
